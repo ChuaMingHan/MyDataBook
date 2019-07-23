94c218f6bfe15a0d6f17a9c7b375d99afde88721
17010265 <GoodBushFD2R>
2019-07-23 22:30:45 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/mydatabook/BioFragment.java
@@ -0,0 +1,34 @@
+package sg.edu.rp.c346.mydatabook;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BioFragment extends Fragment {
+
+
+    public BioFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.biofragment, container, false);
+
+
+    }
+
+}
